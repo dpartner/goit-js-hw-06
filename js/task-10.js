a94f2002440ed2c1ae711a945560f930860733e9
dpartner <d.partner.ua@gmail.com>
2022-03-23 16:42:07 +0200

    10 task completed

--- js/task-10.js
+++ js/task-10.js
@@ -11,3 +11,41 @@ function getRandomHexColor() {
 // 3. Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
 
 // Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
+
+const ref = {
+  input: document.querySelector('input[type="number"]'),
+  create: document.querySelector('[data-create]'),
+  destroy: document.querySelector('[data-destroy]'),
+  container: document.querySelector('#boxes'),
+};
+
+ref.create.addEventListener('click', onCreate);
+ref.destroy.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const allElements = [];
+  let widthValue = 30;
+  let heightValue = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const element = document.createElement('div');
+    element.style.width = `${widthValue}px`;
+    element.style.height = `${heightValue}px`;
+    element.style.background = getRandomHexColor();
+    element.classList.add('square');
+
+    widthValue += 10;
+    heightValue += 10;
+
+    allElements.push(element);
+  }
+  return ref.container.append(...allElements);
+}
+
+function onCreate() {
+  createBoxes(ref.input.value);
+}
+
+function destroyBoxes() {
+  ref.container.innerHTML = '';
+}
